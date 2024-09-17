1fd5412fe5f7c8ade17ff3eb62a1a77a9dd460d9
Isaac Huang <isaac.huang.tw@gmail.com>
2024-09-17 21:29:42 +0800

    Add displayIntro()

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+const chalkAnimation = require('chalk-animation');
+
+const sleep = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function displayIntro() {
+    console.clear();
+    const titleText = 'JSDQ - JavaScript Daily Question';
+    const animatedTitle = chalkAnimation.rainbow(titleText);
+    await sleep();
+    animatedTitle.stop();
+}
+
+async function runQuiz() {
+    await displayIntro();
+}
+
+runQuiz();
